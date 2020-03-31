348f35d2719ab80723f004aa19c5de0af50da416
tylerth <tylerhuts@gmail.com>
2020-03-30 20:29:21 -0400

    adding solution for leetcode problem 9 Palindrome Number

---
+++ palindrome_number.py
@@ -0,0 +1,11 @@
+# 9. Palindrome Number
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        x = str(x)
+        
+        if x == x[::-1]:
+            return True
+        else:
+            return False
