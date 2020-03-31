82db1d3ab5584490fc2d513c9eb957cfb7092134
tylerth <tylerhuts@gmail.com>
2020-03-30 20:32:50 -0400

    updated problem difficulty and description

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,5 +1,10 @@
 # 9. Palindrome Number
+# Difficulty: Easy
 
+'''
+Determine whether an integer is a palindrome. 
+An integer is a palindrome when it reads the same backward as forward.
+'''
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         
